a386c104e98582f7d39c341363f0a2f40c4801b1
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-03-23 00:41:09 -0300

    Maze

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,13 @@ public class PlayerController : MonoBehaviour {
 	public Text countText;
 	public Text winText;
 	private Rigidbody rb;
-	private int count;
+	private int count_batteries;
+	private int total_batteries = 10;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
-		count = 0;
+        count_batteries = 0;
 		SetCountText ();
 		winText.text = "";
 	}
@@ -24,6 +25,11 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+		if (rb.position.y < -10)
+		{
+            rb.position = new Vector3 (0.0f, 0.0f, 0.0f);
+            rb.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+        }
 	}
 
 	void OnTriggerEnter(Collider other) 
@@ -31,15 +37,15 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
 			other.gameObject.SetActive (false);
-			count += 1;
+            count_batteries++;
 			SetCountText();
 		}
 	}
 
 	void SetCountText()
 	{
-		countText.text = "Count: " + count.ToString ();
-		if (count >= 12) 
+		countText.text = "Batteries: " + (total_batteries - count_batteries).ToString();
+		if (count_batteries >= total_batteries)
 		{
 			winText.text = "You Win!";
 		}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float remaining_time = 60.0f;
+    public Text timeText;
+    public Text winText;
+    public GameObject player;
+
+    void Start()
+    {
+        timeText = GameObject.FindGameObjectWithTag("TimeText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        remaining_time -= Time.deltaTime;
+        UpdateTimeText();
+        if (remaining_time <= 0.0f) {
+            player.SetActive(false);
+            timeText.text = "";
+            winText.text = "Too Bad...";
+            if (remaining_time <= -10.0f)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            
+        }
+    }
+
+    void UpdateTimeText()
+    {
+        int minutes = Mathf.FloorToInt(remaining_time / 60);
+        int seconds = Mathf.FloorToInt(remaining_time % 60);
+        string timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeText.text = "Time: " + timeString;
+    }
+}
